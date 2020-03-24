d1c8b7399e962bdeb858426f42a99b8faf9c4961
Kevin Wellmann <kevin@wellmann.io>
2020-03-24 23:27:50 +0100

    refactor component structure

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,8 @@
-export { default as DateTimePickerControl } from './DateTimePickerControl';
+export { default as DateTimePickerControl } from './Control/DateTimePickerControl';
 export { default as EditContext } from './EditContext';
-export { default as getEdit } from './EditWrapper';
-export { default as InspectorControls } from './InspectorControlsWrapper';
-export { default as MediaPlaceholder } from './MediaPlaceholderWrapper';
-export { default as RichText } from './RichTextWrapper';
-export { default as URLControl } from './URLControl';
+export { default as getEdit } from './Wrapper/EditWrapper';
+export { default as InspectorControls } from './Wrapper/InspectorControlsWrapper';
+export { default as MediaPlaceholder } from './Wrapper/MediaPlaceholderWrapper';
+export { default as RichText } from './Wrapper/RichTextWrapper';
+export { default as URLControl } from './Control/URLControl';
 export { default as withWordLimit } from './withWordLimit';
